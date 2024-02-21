6d529e9b0b1c43b61a665e46395cec9fe109f8f6
mlitman <awesomefat@gmail.com>
2024-02-21 10:46:38 -0600

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
 
